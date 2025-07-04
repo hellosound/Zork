bfccd4d0dd39c72cee2e3c8410372e56d3cf0a70
hellosound <81783062+hellosound@users.noreply.github.com>
2025-07-04 15:32:26 -0500

    Add Garden room and basic movement loop to Zork
    Introduces a new Garden room, sets up additional exits between rooms, and implements a simple game loop allowing the player to move by entering directions. Also adds a welcome message and streamlines the main function for interactive play.

--- src/Zork.cpp
+++ src/Zork.cpp
@@ -10,34 +10,46 @@
 
 int main()
 {
-    // ROOM MOVEMENT TEST.
-  
- 
-
     //Create Rooms
     Room* LivingRoom = new Room("Living Room", "a place full of old furniture, you wonder how many stories happened her");
     Room* Kitchen = new Room("Kitchen", "a small kitchen, looks like it hasn't been used in a long time...");
+    Room* Garden = new Room("Garden", " a garden full of plants you've never seen... smells funny though.");
 
 
     //Create Exits
-    Exit* LivingRoomNorthExit = new Exit("NORTH", Kitchen);
-    LivingRoom->AddEntity(LivingRoomNorthExit);
+    Exit* nToKitchen = new Exit("NORTH", Kitchen);
+    Exit* sToLivingRoom = new Exit("SOUTH", LivingRoom);
+    Exit* eToGarden = new Exit("EAST", Garden);
+    Exit* wToKitchen = new Exit("WEST", Kitchen);
+
+
+    //Add Exits
+    LivingRoom->AddEntity(nToKitchen);
+    Kitchen->AddEntity(sToLivingRoom);
+    Kitchen->AddEntity(eToGarden);
+    Garden->AddEntity(wToKitchen);
+
 
     //Add Objects
-
     Entity* bag = new Entity("Bag", "A bag that looks to contain some coins", Entity::ITEM);
     Kitchen->AddEntity(bag);
 
- 
     //CreatePlayer
     Player* mainPlayer = new Player("Jonathan", "A wondering young person", LivingRoom);
 
-    //PLAYER MOVEMENT TEST
-    mainPlayer->TryToMoveToRoom("NORTH");
+    //Welcome Message
+    std::cout << "Welcome to Zork!\n";
 
+    //--Game Loop--//
+    std::string input;
 
-
-
+    while (true)
+    {
+        std::cout << "\nEnter a direction to move (NORTH,EAST,SOUTH,WEST)";
+        std::getline(std::cin, input);
+    
+         mainPlayer->TryToMoveToRoom(input);
+    }
     std::cout << "\nPress Enter to exit...";
     std::cin.get();
 
